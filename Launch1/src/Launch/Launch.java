1c99c4b534b8e89eb9e69ca333bf0320caae2f08
Himanshu <himansh0198@gmail.com>
2024-09-27 10:02:42 +0530

    third commit

--- Launch1/src/Launch/Launch.java
+++ Launch1/src/Launch/Launch.java
@@ -1,11 +1,41 @@
 package Launch;
+class Test{
+	private int age;
+	private String name;
+	private String city;
+void setAge(int age) {
+	this.age=age;
+}
+int getAge() {
+	return age;
+}
+void setName(String name) {
+	this.name=name;
+}
+String getName() {
+	return name;
+}
 
+void setCity(String city) {
+	this.city=city;
+}
+String getCity() {
+	return city;
+
+
+}
 public class Launch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi! There");
-		System.out.println("hello!");
+ 		Test t1=new Test();
+		t1.setAge(12);
+		t1.setName("Himanshu");
+		t1.setCity("Jhansi");
+		System.out.println(t1.getAge());
+		System.out.println(t1.getName());
+		System.out.println(t1.getCity());
+		
 	}
-
+}
 }
